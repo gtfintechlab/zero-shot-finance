28ec44b3945b9255118be2596089b19398fd9252
Glenn Matlin <glennmatlin@users.noreply.github.com>
2023-07-28 18:04:15 -0400

    fix error with logger for each seed run, fix the output path directory stuff, change some loggers from info to debug

--- src/dolly/run.py
+++ src/dolly/run.py
@@ -45,24 +45,29 @@ def main(args):
     generate_text = InstructionTextGenerationPipeline(model=model, tokenizer=tokenizer)
 
     for seed in tqdm(SEEDS):
-        logger = setup_logger(f"seed_{seed}")
+        logger.info(f"Running inference for seed {seed}")
+
         # assign seed to numpy and PyTorch
         torch.manual_seed(seed)
         np.random.seed(seed)
 
         start_t = time()
-        # load test data
-        # TODO: add task name to data path
-        TEST_DATA = ROOT_DIRECTORY / "data" / "test"
-        TEST_DATA.mkdir(parents=True, exist_ok=True)
-        test_data_path = TEST_DATA / f"{data_category}-test-{seed}.xlsx"
-        logger.info(f"Loading test data from {test_data_path}")
-        data_df = pd.read_excel(test_data_path)
 
+        # TODO: I shouldn't need to make these directories, they should exist -- if they dont, throw an error!
+        DATA_DIRECTORY = ROOT_DIRECTORY / "data"
+        DATA_DIRECTORY.mkdir(parents=True, exist_ok=True)
+        TASK_DIRECTORY = DATA_DIRECTORY / args.task_name
+        TASK_DIRECTORY.mkdir(parents=True, exist_ok=True)
+        TEST_DIRECTORY = TASK_DIRECTORY / "test"
+        TEST_DIRECTORY.mkdir(parents=True, exist_ok=True)
+        test_data_fp = TEST_DIRECTORY / f"{data_category}-test-{seed}.xlsx"
+
+        logger.info(f"Loading test data from {test_data_fp}")
+        data_df = pd.read_excel(test_data_fp)
         sentences = data_df["sentence"].to_list()
-        logger.info(f"Number of sentences: {len(sentences)}")
+        logger.debug(f"Number of sentences: {len(sentences)}")
         labels = data_df["label"].to_numpy()
-        logger.info(f"Number of labels: {len(labels)}")
+        logger.debug(f"Number of labels: {len(labels)}")
 
         prompts_list = []
         for sen in tqdm(sentences, desc="Generating prompts"):
@@ -71,12 +76,12 @@ def main(args):
 
         logger.info("Prompts generated. Running model inference...")
         res = generate_text(prompts_list)
-        logger.info("Model inference completed. Processing outputs...")
 
+        logger.info("Model inference completed. Processing outputs...")
         output_list = []
         for i in range(len(res)):
             output_list.append([labels[i], sentences[i], res[i][0]["generated_text"]])
-        logger.info(f"Number of outputs: {len(output_list)}")
+        logger.debug(f"Number of outputs: {len(output_list)}")
 
         results = pd.DataFrame(
             output_list, columns=["true_label", "original_sent", "text_output"]
@@ -84,7 +89,7 @@ def main(args):
         time_taken = int((time() - start_t) / 60.0)
         logger.info(f"Time taken: {time_taken} minutes")
         PROMPT_OUTPUTS = (
-            TEST_DATA / "llm_prompt_outputs" / args.task_name / args.quantization
+                TEST_DIRECTORY / "llm_prompt_outputs" / args.task_name / args.quantization
         )
         PROMPT_OUTPUTS.mkdir(parents=True, exist_ok=True)
         results_fp = f"dolly_{seed}_{TODAY.strftime('%d_%m_%Y')}_{time_taken}.csv"
